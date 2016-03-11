08fd18c9638954940a839e3ec6ef9ba587357f6b
Mike Lee <git@iskitz.com>
2016-03-10 22:27:25 -0800

    wakatta: Added new and reordered existing ToDos

--- kana.game/index.js
+++ kana.game/index.js
@@ -20,18 +20,40 @@
 
     , わかった:
         [ "rename kana.game folder to wakatta"
-        , "swipe left slows display, right quickens it"
         , "swipe up visualizes Yes, down visualizes No"
-        , "auto speed up or down per user's success speed"
         , "enable pairing hiragana, katakana, + romaji"
         , "add learn mode that only shows right answers"
+        , "swipe left slows display, right quickens it"
+        , "auto speed up or down per user's success speed"
+
+
+        , { "save skill progress":
+              [ "temporarily using SessionStorage or Cookies"
+              , "persistently using LocalStorage, IndexedDB, "
+              ]
+          }
+
         , "add romaji typing practice"
         , "add vocabulary, group by user's role(s) + interest(s)"
         , "add image: word pairing"
         , "add pronounciation audio: gendered voices"
         , "add statement confirmation"
         , "add image(s): statement pairing"
-        , "add character writing practice"
+
+        , { perf:
+              [ "test after each feature change"
+              , "use http://webpagetest.org/"
+
+              , { "http://tinyurl.com/wakatta-2016-03-10 http://tinyurl.com/wakatta-2016-03-10-noanimation":
+                    [ "1000 ms between 1st request + 1st render + animation ? css animation, js execution"
+                    , " 750 ms between 1st request + 1st render - animation ? looks like favicon"
+                    , " 536 ms idle time                                    ? why no activity"
+                    , " 706 ms between 1st render  + favicon available      ? why no main thread activity"
+                    , " 590 ms between 1st request + favicon requested      ? store as data-url to minimize"
+                    ]
+                }
+              ]
+          }
 
         , { "proficiency progress":
               [ "per character"
@@ -52,6 +74,18 @@
                 }
               , "combine letters; i.e. ひろこ : ヒロコ"
               , "improve speed via auto-scroll and fade"
+              ]
+          }
+
+        , { "add character writing practice":
+              [ "look for existing web solutions"
+              , "? use character svgs and canvas input"
+              ]
+          }
+
+        , { "add speech practice":
+              [ "look for existing web solutions"
+              , "? use webrtc/microphone api and audio analysis"
               ]
           }
        ] //+.わかった
