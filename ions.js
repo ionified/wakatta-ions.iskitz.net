dac3a34c53c1ea9ddbfad26806297d9c0d92644a
Mike Lee <git@iskitz.com>
2016-12-03 06:44:39 -0800

    +/ Confirmed that ionify.0.1's +{log_level:boolean} action works / +/ Added mysrc script attribute for convenient local development /

--- ions.js
+++ ions.js
@@ -2,16 +2,17 @@
 
 +
 { re:
-    { id: "wakatta@ions.iskitz.net"
-    , by: "Michael Lee"
-    , at: "2016.12.01...08.19-07"
+    { id: "wakatta@ions.iskitz.net.0.1.1"
+    , by: "mike.lee@iskitz"
+    , at: "2016.12.03-08...08.19-07"
     , in:
         [ "forest-hills.new-york.usa.earth"
         , "san-jose.california.usa.earth"
         ]
-    },
+    }
 
-  get: ["game"]
+, get  : ["game"]
+, debug: !true
 }
 
 ;
