eadbda93c62a59590998c1a784fce5d667f80e89
Mike Lee <git@iskitz.com>
2017-04-04 08:20:51 -0700

    ~/ Added +vision for exploring wakatta's game ideas /
    +/ Legibility++ of ionify's script tag in index.html/

---
+++ vision.js
@@ -0,0 +1,19 @@
+;
+
+~
+[` Beginner:
+
+   See alphabet in gojuon order:
+
+   あ い う え お
+   ア イ ウ エ オ
+   a  i  u e  o
+
+   Choose when + what to be tested:
+
+   n.Displaying as flashcards
+   q.How does user start testing?
+ `
+]
+
+;
